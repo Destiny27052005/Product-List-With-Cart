10d44244daa7df6b8b120aba32725789de236d12
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-02-20 19:54:36 +0100

    Add to cart

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react"
 
 function Card({ product }) {
+    const [isSelected, setIsSelected] = useState(false);
+    const [num, setNum] = useState(0);
 
+    const handleChange = () => {
+        setIsSelected(true)
+    }
 
+    const borderClass = isSelected ? "border-[#c73a0f] border-3" : "border-transparent border-2";
 
     return (
         <div className="my-5">
             <div className="relative mb-8">
-                <img className="block rounded-2xl md:hidden" src={product.image.mobile} alt={product.name} />
-                <img className="hidden md:block lg:hidden" src={product.image.tablet} alt={product.name} />
-                <img className="hidden lg:block" src={product.image.desktop} alt={product.name} />
-                <div className="absolute place-self-center -bottom-6 bg-white py-2 px-3 rounded-3xl border-[#ad8985] border-2 cursor-pointer">
-                    <p className="text-2xl font-medium">Add to Cart</p>
+                <img className={`block rounded-2xl md:hidden ${borderClass}`} src={product.image.mobile} alt={product.name} />
+                <img className={`hidden md:block lg:hidden ${borderClass}`} src={product.image.tablet} alt={product.name} />
+                <img className={`hidden lg:block ${borderClass}`} src={product.image.desktop} alt={product.name} />
+                <div className={`absolute place-self-center -bottom-6 ${isSelected ? "bg-[#c73a0f]" : "bg-white"} py-2 px-3 rounded-3xl border-[#ad8985] border-[1.5px] cursor-pointer`} onClick={handleChange}>
+                    {
+                        isSelected ?
+                            <div className="w-full h-full text-2xl font-medium text-white flex items-center gap-5">
+                                <img src="/images/icon-decrement-quantity.svg" alt="Decrement" onClick={() => setNum(prev => Math.max(0, prev - 1))} />
+                                {num}
+                                <img src="/images/icon-increment-quantity.svg" alt="Increment" onClick={() => setNum(prev => prev + 1)} />
+                            </div> :
+                            <p className="text-2xl font-medium">Add to Cart</p>
+                    }
+
                 </div>
             </div>
             <h2 className='text-[#87635a] my-2'>{product.category}</h2>
--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -3,11 +3,11 @@
 function Cart() {
 
     return (
-        <>
+        <div className="bg-white grid text-center place-content-center mx-auto mt-5 rounded-t-2xl">
             <h1>Your Card(0)</h1>
-            <img src="images/illustration-empty-cart.svg" alt="empty cart" />
+            <img className="place-self-center" src="images/illustration-empty-cart.svg" alt="empty cart" />
             <p>Your added items will appear here</p>
-        </>
+        </div>
     )
 }
 
